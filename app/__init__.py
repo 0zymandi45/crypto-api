a299fb698b56edc59f32ba832367a0359eae593c
0zymandi45 <erik.kullack@gmail.com>
2024-11-11 14:43:12 -0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,12 @@
-# Initialize Flask app
+from flask import Flask
+from app.config.config import Config
+from app.routes.api_routes import api_blueprint
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    # Register blueprints
+    app.register_blueprint(api_blueprint, url_prefix="/api")
+
+    return app
