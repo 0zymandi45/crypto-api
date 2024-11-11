e2729e63d850bde91a5606fbe547ad46bca1aded
0zymandi45 <erik.kullack@gmail.com>
2024-11-11 14:47:16 -0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,1 +1,6 @@
-# App configuration and constants
+import os
+
+class Config:
+    SECRET_KEY = os.getenv("SECRET_KEY", "your_secret_key")
+    MONGODB_URI = os.getenv("MONGODB_URI", "mongodb://localhost:27017/your_db")
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "jwt_secret")
