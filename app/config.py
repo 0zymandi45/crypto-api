4d7563b5a884e461a2fa17a6e41889de6a6313f5
0zymandi45 <erik.kullack@gmail.com>
2024-11-11 15:13:10 -0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,6 +1,19 @@
 import os
+from datetime import timedelta
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "your_secret_key")
-    MONGODB_URI = os.getenv("MONGODB_URI", "mongodb://localhost:27017/your_db")
-    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "jwt_secret")
+    SECRET_KEY = os.getenv("SECRET_KEY", "your_default_secret_key")
+    
+    # Configuração do Redis
+    REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
+    REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
+    REDIS_EXPIRATION = timedelta(minutes=int(os.getenv("REDIS_EXPIRATION_MINUTES", 10)))
+    
+    # Configuração de MongoDB (caso necessário)
+    MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/crypto_db")
+    
+    # Configuração da API externa
+    API_URL = os.getenv("API_URL", "https://api.mercadobitcoin.net/api/v4/ticker")
+
+config = Config()
+
