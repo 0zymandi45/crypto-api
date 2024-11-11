8d4324883c150a9571353a6a27aa88c0dc358d59
0zymandi45 <erik.kullack@gmail.com>
2024-11-11 15:04:58 -0300

    Update auth.py

--- app/auth.py
+++ app/auth.py
@@ -1,1 +1,17 @@
-# Handle JWT auth
+from flask import request, jsonify
+from functools import wraps
+import jwt
+from config import SECRET_KEY
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = request.args.get('token')
+        if not token:
+            return jsonify({'message': 'Token is missing!'}), 403
+        try:
+            data = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+        except:
+            return jsonify({'message': 'Token is invalid!'}), 403
+        return f(*args, **kwargs)
+    return decorated
